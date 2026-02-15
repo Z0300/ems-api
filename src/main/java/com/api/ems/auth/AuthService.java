212176102af8a1ba4f0b8063dd176dddfd2826bd
subZero <marvinfordan08@gmail.com>
2026-02-15 11:52:43 +0800

    auth - implements login and refresh endpoints

---
+++ src/main/java/com/api/ems/auth/AuthService.java
@@ -0,0 +1,21 @@
+package com.api.ems.auth;
+
+import com.api.ems.entities.User;
+import com.api.ems.users.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var userId = (Long) Objects.requireNonNull(authentication).getPrincipal();
+        return userRepository.findById(Objects.requireNonNull(userId)).orElse(null);
+    }
+}
--- src/main/java/com/api/ems/common/Jwt.java
+++ src/main/java/com/api/ems/common/Jwt.java
@@ -3,18 +3,15 @@ package com.api.ems.common;
 import com.api.ems.entities.enums.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import lombok.AllArgsConstructor;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@AllArgsConstructor
 public class Jwt {
     private final Claims claims;
     private final SecretKey secretKey;
-
-    public Jwt(Claims claims, SecretKey secretKey) {
-        this.claims = claims;
-        this.secretKey = secretKey;
-    }
 
     public boolean isExpired() {
         return claims.getExpiration().before(new Date());
